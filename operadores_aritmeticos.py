5bce81c26562fd07b7bb67c2a15482034c1c7ae6
EscalanteUCB <eescalante@ucb.edu.bo>
2025-04-10 09:50:03 -0400

    primer codigo en py
    operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,17 @@
+
+#CREAR UNA VAR
+saludo = "Hola Mundo !!!"
+print(saludo)
+
+
+#suma de numeros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print(suma)
+
+#suma de palabras -> concatenar palabras
+palabra1 = "hola"
+palabra2 = "mundo"
+suma_palabras = palabra1 + " " + palabra2
+print(suma_palabras)
